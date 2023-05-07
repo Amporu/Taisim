a5811bbd4e0aa442a4aea8ef19d3efbfdb16021a
Amporu <Tucudean.Adrian.Ionut@outlook.com>
2023-05-08 01:03:05 +0300

    dsadadsadsaads

---
+++ src/sparkverse/external_data.py
@@ -0,0 +1,10 @@
+
+import os
+CAR    = os.path.join(os.path.dirname(__file__), 'sparkworld', 'data/player.png')
+LEVEL1 = os.path.join(os.path.dirname(__file__), 'sparkworld', 'data/LineFollower1.png')
+LEVEL2 = os.path.join(os.path.dirname(__file__), 'sparkworld', 'data/LineFollower2.png')
+LEVEL3 = os.path.join(os.path.dirname(__file__), 'sparkworld', 'data/LaneKeeper1.png')
+LEVEL4 = os.path.join(os.path.dirname(__file__), 'sparkworld', 'data/LaneKeeper2.png')
+LEVEL5 = os.path.join(os.path.dirname(__file__), 'sparkworld', 'data/LaneKeeper3.png')
+LEVEL6 = os.path.join(os.path.dirname(__file__), 'sparkworld', 'data/Cropps1.png')
+LEVEL7 = os.path.join(os.path.dirname(__file__), 'sparkworld', 'data/Cropps2.png')
