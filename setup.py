bd9f6e332be370fb39187302067e2f25d82c58d6
Amporu <Tucudean.Adrian.Ionut@outlook.com>
2023-05-18 00:27:54 +0300

    added pydoc, pystring documentation,dealt with E/W

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.0.1'
+version = '0.0.4'
 
 install_requires = [
     # List your project dependencies here.
--- src/sparkverse/utils.py
+++ src/sparkverse/utils.py
@@ -1,58 +1,109 @@
+"""
+info:This module contains a collection of non categorizable functions
+autor: Tucudean Adrian-Ionut
+date: 17.05.2023
+email: Tucudean.Adrian.Ionut@outlook.com
+license: MIT
+"""
+
 import cv2
 import pygame
 import numpy as np
 
 class Utils:
-    """User friendly platform + other esential functions"""
+    """a class where i dump yet non categorizable functions"""
     x,y=0,0
     rotation=0
     speed=0
-    font = cv2.FONT_HERSHEY_SIMPLEX
+    font = cv2.FONT_HERSHEY_SIMPLEX   #pylint: disable=E1101
     keys=["Q","H","R","W","A","S","D"]
-    description=["Quit","Hide/Show HelpBar","Reccord","Move Forward","Move Left","Move Backward","Move Right"]
+    description=["Quit","Hide/Show",  #pylint: disable=C0301
+                 "Reccord",
+                 "Move Forward",
+                 "Move Left",
+                 "Move Backward",
+                 "Move Right"]
+    @staticmethod
     def scale_image(img, factor):
+        """
+        function designated for scaling an image by a factor
+
+        Parameters:
+        img (Mat)    : source image 
+        factor (int) : scale factor (where 1 is the original size)
+
+        Returns: 
+        Mat         : rescaled image
+        """
         size = round(img.get_width() * factor), round(img.get_height() * factor)
         return pygame.transform.scale(img, size)
 
+    @staticmethod
+    def blit_rotate_center(win, image, point, angle):
+        """
+        rotate images relative to a point
 
-    def blit_rotate_center(win, image, top_left, angle):
-        """rotate images relative to a point """
+        Parameters:
+        win (pygameObj)    : pygame window object
+        image (Mat)        : desired image
+        point (tuple)      : point of rotation
+        angle (int)        : the angle of rotation
+        """
         rotated_image = pygame.transform.rotate(image, angle)
-        #new_rect = rotated_image.get_rect(center=image.get_rect(topleft=top_left).center)
-        win.blit(rotated_image, top_left)
+        win.blit(rotated_image, point)
 
-
+    @staticmethod
     def blit_text_center(win, font, text):
-        """ add text to pygame image object"""
+        """
+        add text to pygame image object
+
+        Parameters: 
+        win (pygameObj)    :  pygame window object
+        font (int)         :  desired font 
+        text (string)      :  text message
+        
+        """
         render = font.render(text, 1, (200, 200, 200))
         win.blit(render, (win.get_width()/2 - render.get_width() /
                       2, win.get_height()/2 - render.get_height()/2))
-    def Display(frame):
-        """opencv lookalike Display function with user friendly helpBar"""
-        h,w,c=frame.shape
-        image = np.ones((h, w//2, c), dtype=np.uint8)*255
+    @staticmethod
+    def display(frame):
+        """
+        HelpBar display
+
+        Parameters:
+        frame (Mat): input frame
+        """
+        height,width,channels=frame.shape
+        image = np.ones((height, width//2, channels), dtype=np.uint8)*255
         if HelpBar.show==1:
-            cv2.putText(image, "HelpBar", (10, 20), Utils.font, 1, (9,0,0), 2)
-            for i in range(len(Utils.keys)):
-                cv2.rectangle(image,(10,30+i*40),(40,60+i*40),(255,0,0),3)
-                cv2.putText(image,Utils.keys[i],(20,50+i*40),Utils.font,0.5,(0,0,255),2)
-                cv2.putText(image,Utils.description[i],(50,50+i*40),Utils.font,0.5,(0,0,255),2)
-            cv2.putText(image,"Car PROPERTIES",(10,380),Utils.font,1,(0,0,0),2)
-            cv2.rectangle(image,(10,400),(310,700),(255,0,255),2)
-            cv2.putText(image,"Position",(30,430),Utils.font,0.5,(255,0,0),2)
-            cv2.putText(image,"X = "+str(Utils.x),(30,450),Utils.font,0.5,(0,0,0),2)
-            cv2.putText(image,"Y = "+str(Utils.y),(130,450),Utils.font,0.5,(0,0,0),2)
-            cv2.putText(image,"Angular Movement",(30,470),Utils.font,0.5,(255,0,0),2)
-            cv2.putText(image,"angle = "+str(Utils.rotation),(30,490),Utils.font,0.5,(0,0,0),2)
-            cv2.putText(image,"Linear Movement",(30,510),Utils.font,0.5,(255,0,0),2)
-            
-            cv2.putText(image,"speed = "+str(round(Utils.speed,2)),(30,530),Utils.font,0.5,(0,0,0),2)
+            HelpBar.showpannel(frame,image)
+class HelpBar:
+    """class for controlling HelpBar menu"""
+    show = 0
+    merg = 0
+    @staticmethod
+    def showpannel(frame,image):
+        """
+        Help Bar GUI
 
-            frame=cv2.hconcat([frame,image])
-            
-        
-        cv2.imshow("FramexMap",frame)
-class HelpBar():
-    #image = np.ones((720, 100, 3), dtype=np.uint8)*255
-
-    show=0
+        Parameters:
+        frame (Mat): simulator frame
+        image (Mat): blank HelpBar frame
+        """
+        cv2.putText(img=image,text="HelpBar",org=(10, 20),fontFace=Utils.font,fontScale=1,color=(9,0,0),thickness=2)#pylint: disable=E1101
+        for i in range(end=len(Utils.keys)):
+            cv2.rectangle(img=image,pt1=(10,30+i*40),pt2=(40,60+i*40),color=(255,0,0),thickness=3)#pylint: disable=E1101
+            cv2.putText(img=image,text=Utils.keys[i],org=(20,50+i*40),fontFace=Utils.font,fontScale=0.5,color=(0,0,255),thickness=2)#pylint: disable=E1101
+            cv2.putText(img=image,text=Utils.description[i],org=(50,50+i*40),fontFace=Utils.font,fontScale=0.5,color=(0,0,255),thickness=2)#pylint: disable=E1101
+        cv2.putText(img=image,text="Car PROPERTIES",org=(10,380),fontFace=Utils.font,fontScale=1,color=(0,0,0),thickness=2)#pylint: disable=E1101
+        cv2.rectangle(img=image,pt1=(10,400),pt2=(310,700),color=(255,0,255),thickness=2)#pylint: disable=E1101
+        cv2.putText(img=image,text="Position",org=(30,430),fontFace=Utils.font,fontScale=0.5,color=(255,0,0),thickness=2)#pylint: disable=E1101
+        cv2.putText(img=image,text="X = "+str(Utils.x),org=(30,450),fontFace=Utils.font,fontScale=0.5,color=(0,0,0),thickness=2)#pylint: disable=E1101
+        cv2.putText(img=image,text="Y = "+str(Utils.y),org=(130,450),fontFace=Utils.font,fontScale=0.5,color=(0,0,0),thickness=2)#pylint: disable=E1101
+        cv2.putText(img=image,text="Angular Movement",org=(30,470),fontFace=Utils.font,fontScale=0.5,color=(255,0,0),thickness=2)#pylint: disable=E1101
+        cv2.putText(img=image,text="angle = "+str(Utils.rotation),org=(30,490),fontFace=Utils.font,fontScale=0.5,color=(0,0,0),thickness=2)#pylint: disable=E1101
+        cv2.putText(img=image,text="Linear Movement",org=(30,510),fontFace=Utils.font,fontScale=0.5,color=(255,0,0),thickness=2) #pylint: disable=E1101          
+        cv2.putText(img=image,text="speed = "+str(round(Utils.speed,2)),org=(30,530),fontFace=Utils.font,fontScale=0.5,color=(0,0,0),thickness=2)#pylint: disable=E1101
+        frame=cv2.hconcat([frame,image])#pylint: disable=E1101
+        cv2.imshow("FramexMap",frame)#pylint: disable=E1101
